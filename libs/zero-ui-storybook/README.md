d3f74ad210238570cf2df6c7f90caec9cf275669
zerok-cell <swiftkey382@gmail.com>
2025-04-10 20:54:02 +0300

    Налажен storybook. Вынесен components в отдельную либу

---
+++ libs/zero-ui-storybook/README.md
@@ -0,0 +1,8 @@
+# @zero-ui/zero-ui-storybook
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test @zero-ui/zero-ui-storybook` to execute the unit tests via
+[Vitest](https://vitest.dev/).
---
+++ libs/zero-ui-storybook/src/Button.stories.tsx
@@ -0,0 +1,25 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { within } from '@storybook/testing-library';
+import { expect } from '@storybook/jest';
+import { Button } from '@zero-ui/components';
+
+const component = Button;
+
+const meta: Meta<typeof component> = {
+  component: component,
+  title: 'Button',
+};
+export default meta;
+type Story = StoryObj<typeof component>;
+
+export const Primary = {
+  args: {},
+};
+
+export const Heading: Story = {
+  args: {},
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    expect(canvas.getByText(/Welcome to Button!/gi)).toBeTruthy();
+  },
+};
